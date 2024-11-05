f601bab5186425fd3db7ce123274cc410c16bc9a
NextZtepS <natdanaiongarjvaja@gmail.com>
2024-11-04 21:41:28 -0800

    initial commit

--- mongo_backup.sh
+++ mongo_backup.sh
@@ -7,15 +7,15 @@ BACKUP_USER="root"                              # Username for backup VPS
 BACKUP_HOST="172.0.0.1"                         # IP or hostname of the backup VPS
 SSH_KEY="$HOME/.ssh/mongo_backup_key"           # Path to SSH private key
 
-# Step 1: Create a MongoDB dump
+# Create a MongoDB dump
 echo "Creating MongoDB dump on primary VPS..."
 mongodump --archive=$DUMP_FILE --gzip
 
-# Step 2: Transfer the dump to the backup VPS using SFTP
+# Transfer the dump to the backup VPS using SFTP
 echo "Transferring dump to backup VPS..."
 sftp -i $SSH_KEY $BACKUP_USER@$BACKUP_HOST:$REMOTE_PATH <<< $'put '"$DUMP_FILE"
 
-# Step 3: Clean up the dump file from the primary VPS
+# Clean up the dump file from the primary VPS
 echo "Cleaning up dump file on this primary VPS..."
 rm $DUMP_FILE
 
--- mongo_serve_backup.sh
+++ mongo_serve_backup.sh
@@ -3,11 +3,11 @@
 # Define variables
 LOCAL_BACKUP_PATH="/tmp/primary_mongo_dump.archive"     # Path to the local backup file
 
-# Step 1: Restore the backup to MongoDB
+# Restore the backup to MongoDB
 echo "Restoring backup to MongoDB on secondary server..."
 mongorestore --archive="$LOCAL_BACKUP_PATH" --gzip
 
-# Step 3: Clean up the dump file from the primary VPS
+# Clean up the dump file from the primary VPS
 echo "Cleaning up dump file from primary VPS..."
 rm  $LOCAL_BACKUP_PATH
 
